ee2f4d4fd62e7c6a29f1db48e211370af9adffc9
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-03-19 14:08:50 -0400

    Adding in the js and css files along with adding in the footer and navigation as partials

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
-
+const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
-
 const port = process.env.PORT || 4000;
 
+// Express middleware for serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setting up the express engine to use handlebars as the view
+app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+
+// Setting up the express middleware for setting the view engine
+app.set('view engine', 'handlebars');
+
+// Setting up the main index route
+app.get('/', (req, res) => {
+  res.render('home/index');
+});
+
+// Setting up the about page route
+app.get('/about', (req, res) => {
+  res.render('home/about');
+});
+
+// Set the app to listen on a specific port
 app.listen(port, err => {
   if (err) {
     throw err;
