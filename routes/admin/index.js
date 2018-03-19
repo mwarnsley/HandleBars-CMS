ed25082f2cf64bf973ec8fa54b66ae75f4f9f729
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-03-19 14:45:08 -0400

    Editing the sidebar name to be uniformed. Also setting up more of them handlebars routes and views

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('/*', (req, res, next) => {
+  req.app.locals.layout = 'admin';
+  next();
+});
+
 // Setting up the main admin index route
 router.get('/', (req, res) => {
   res.render('admin/index');
